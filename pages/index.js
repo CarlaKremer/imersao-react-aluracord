1b3e6b3c9b71bb3e0ce2a19126c6305d9d30968c
CarlaKremer <carlakremerf@gmail.com>
2022-01-25 15:59:21 -0300

    ajustes cores

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ function GlobalStyle() {
               width: '100%', maxWidth: '700px',
               borderRadius: '5px', padding: '32px', margin: '16px',
               boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-              backgroundColor: appConfig.theme.colors.neutrals[700],
+              backgroundColor: appConfig.theme.colors.neutrals[600],
             }}
           >
             {/* Formulário */}
@@ -86,7 +86,7 @@ function GlobalStyle() {
               }}
             >
               <Titulo tag="h2">Boas vindas de volta!</Titulo>
-              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[100] }}>
                 {appConfig.name}
               </Text>
   
@@ -95,9 +95,9 @@ function GlobalStyle() {
                 textFieldColors={{
                   neutral: {
                     textColor: appConfig.theme.colors.neutrals[200],
-                    mainColor: appConfig.theme.colors.neutrals[900],
+                    mainColor: appConfig.theme.colors.neutrals[950],
                     mainColorHighlight: appConfig.theme.colors.primary[500],
-                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                    backgroundColor: appConfig.theme.colors.neutrals[600],
                   },
                 }}
               />
@@ -124,7 +124,7 @@ function GlobalStyle() {
                 alignItems: 'center',
                 maxWidth: '200px',
                 padding: '16px',
-                backgroundColor: appConfig.theme.colors.neutrals[800],
+                backgroundColor: appConfig.theme.colors.neutrals[700],
                 border: '1px solid',
                 borderColor: appConfig.theme.colors.neutrals[999],
                 borderRadius: '10px',
@@ -157,75 +157,5 @@ function GlobalStyle() {
       </>
     );
   }
-// function GlobalStyle() {
-//     return (
-//       <style global jsx>{`
-//         * {
-//           margin: 0;
-//           padding: 0;
-//           box-sizing: border-box;
-//           list-style: none;
-//         }
-//         body {
-//           font-family: 'Open Sans', sans-serif;
-//         }
-//         /* App fit Height */ 
-//         html, body, #__next {
-//           min-height: 100vh;
-//           display: flex;
-//           flex: 1;
-//         }
-//         #__next {
-//           flex: 1;
-//         }
-//         #__next > * {
-//           flex: 1;
-//         }
-//         /* ./App fit Height */ 
-//       `}</style>
-//     );
-// }
 
-// function Title(props){
-//     console.log(props)
-//     const Tag = props.tag
-//     return(
-//         <>
-//             <Tag>{props.children}</Tag>
-//             {/* css in js: */}
-//             <style jsx>{`
-//             ${Tag}{
-//                 color:${appConfig.theme.colors.neutrals[`800`]};
-//                 font-size: 24px;
-//                 font-weight: 600;
-//             }
-//             `}</style>
-//         </>
-//     )
-// }
-
-// //componente react
-// function HomePage() {
-//     //jsx
-//     return (
-//         <div>
-//             <GlobalStyle/>
-//             <Title tag="h2">Boas vindas de volta!</Title>
-//             <h2>Discord - Alura Matrix</h2>
-//             <TextField
-//             label="Nome"
-//             maxLength={0}
-//             name="NameInput"
-//             onChange={function noRefCheck(){}}
-//             onKeyPress={function noRefCheck(){}}
-//             placeholder="Placeholder text..."
-//             rounded="md"
-//             value=""
-//             variant="basicBordered"
-//             />
-//         </div>
-//     )
-// }
-  
-//   export default HomePage
   
