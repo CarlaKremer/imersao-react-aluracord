26e84a90922158cdc77df1619eb82419018e468f
CarlaKremer <carlakremerf@gmail.com>
2022-01-25 20:01:48 -0300

    segundo dia

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,8 @@
 import appConfig from "../config.json"
-// JavaScript
 import { Box, Button, Text, TextField, Image  } from '@skynexui/components';
+import React from "react";
+import {useRouter} from "next/router";
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
   
   function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -48,11 +21,13 @@ function GlobalStyle() {
   }
   
   export default function PaginaInicial() {
-    const username = 'CarlaKremer';
-  
+    const [username, setUsername ]= React.useState('');
+    // valor que definiu e funcção que muda o valor
+    const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
+        
         {/* Background */}
         <Box 
           styleSheet={{
@@ -80,6 +55,15 @@ function GlobalStyle() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={
+                function(infosDoEvento){
+                  infosDoEvento.preventDefault();
+                  console.log("submeteu")
+                  // window.location.href="/chat"
+                  roteamento.push("/chat");
+                }
+
+              }
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -91,6 +75,18 @@ function GlobalStyle() {
               </Text>
   
               <TextField
+                value={username} 
+                onChange={
+                    function(event)
+                    { 
+                      //Onde ta o valor?:
+                      console.log("usuario digitou", event.target.value)
+                      //troca o valor da variavel
+                      const valor = event.target.value;
+                      //avisa a função que deve ser trocado
+                      setUsername(valor);
+                    }
+                  }
                 fullWidth
                 textFieldColors={{
                   neutral: {
