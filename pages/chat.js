754dceae218d6b073b0140948a7154748b22690e
CarlaKremer <carlakremerf@gmail.com>
2022-01-27 23:33:58 -0300

    +página do chat

--- pages/chat.js
+++ pages/chat.js
@@ -1,8 +1,211 @@
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React from 'react';
+import appConfig from '../config.json';
 
-export default function PaginaDoChat(){
-    return(
-        <div>
-            Página do chat
-        </div>
+
+export default function ChatPage() {
+    const [mensagem, setMensagem] = React.useState();
+    const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+    /*
+    // Usuário
+    - digita a mensagem no campo
+    - enter envia
+    - adiciona o texto na listagem
+
+    //Dev
+    - criar o campo
+    - usar onChange para e useState para limpar a variavel
+    - listar mensagens
+    */
+    function handleNovaMensagem(novaMensagem) {
+        const mensagem = {
+            texto: novaMensagem,
+            id: listaDeMensagens.length + 1,
+            de: 'carlaKremer'
+
+        }
+        setListaDeMensagens([
+            mensagem,
+            ...listaDeMensagens
+        ]);
+        setMensagem('');
+    }
+    return (
+        <Box
+            // Fundo
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.primary[500],
+                backgroundImage: `url(https://preview.redd.it/g13rkq5m66c81.jpg?width=4096&format=pjpg&auto=webp&s=b9f74e6ab42a832daf4b5d2e40eec361e01ce267)`,
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: appConfig.theme.colors.neutrals['000']
+            }}
+        >
+            <Box
+                // caixa de fora 
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: appConfig.theme.colors.neutrals[700],
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+
+                }}
+            >
+                <Header /* "chat" e logout*/ />
+                <Box
+                    // caixa de dentro
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[600],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+                    {/* // teste
+                    Ta mudando o valor: {mensagem} */}
+                    {/* lista:{listaDeMensagens.map((mensagemAtual)=>{
+                        return( 
+                            <li key={mensagemAtual.id}>
+                                    {mensagemAtual.de}: {mensagemAtual.texto}
+                            </li>
+                        )
+                    })} */}
+                    {/* <MessageList mensagens={[]} /> */}
+                    <MessageList mensagens={listaDeMensagens} />
+                    <Box
+                        //caixa do campo de mensagem
+                        as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            //input
+                            value={mensagem}
+                            onChange={
+                                (event) => { //arrow function
+                                    const valor = event.target.value;
+                                    setMensagem(valor);
+                                }
+                            }
+
+                            onKeyPress={(event) => {
+                                if (event.key === 'Enter') {
+                                    event.preventDefault();
+                                    handleNovaMensagem(mensagem);
+                                }
+                            }}
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+function Header() {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    console.log('MessageList', props);
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'scroll',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+            {props.mensagens.map((mensagens) => {
+                return (
+                    <Text
+                        key={mensagens.id}
+                        tag="li"
+                        styleSheet={{
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                            }
+                        }}
+                    >
+                        <Box
+                            styleSheet={{
+                                marginBottom: '8px',
+                            }}
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '20px',
+                                    height: '20px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/${mensagens.de}.png`}
+                            />
+                            <Text tag="strong">
+                                {mensagens.de}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: appConfig.theme.colors.neutrals[300],
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                        </Box>
+                        {mensagens.texto}
+                    </Text>
+                )
+            })}
+
+        </Box>
     )
 }
